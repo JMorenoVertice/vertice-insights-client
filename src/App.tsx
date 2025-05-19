ad8eb0b6d196396837f84ca8834c912d2198499c
Coral Jácome García <cjacome@grupovertice.com>
2025-05-19 12:22:17 +0200

    Testing2 Fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-import { Grommet, Box } from 'grommet';
-import { Header } from './components/Header';
-import { Main } from './components/Main';
-import { Footer } from './components/Footer';
-import { CoralDev } from './components/CoralDev';
-import { AboutUs } from './components/AboutUs';
-
-const theme = {
-  global: {
-    colors: {
-      brand: '#6200c5',
-      background: 'linear-gradient(90deg, #6200c5 0%, #ff27fc 300%)',
-    },
-    font: {
-      family: 'Arial',
-      size: '18px',
-      height: '20px',
-    },
-  },
-};
-
-function App() {
-  return (
-    <Grommet theme={theme} full>
-      <Box
-        fill
-        background="linear-gradient(135deg, #6200c5 0%, #ff27fc 600%)"
-        style={{
-          minHeight: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <CoralDev />
-        <Box flex="grow" width="100%">
-          <Main />
-          <AboutUs />
-        </Box>
-        <Footer />
-      </Box>
-    </Grommet>
-  );
-}
-export default App;
-=======
 // src/App.tsx
 import React, { useEffect, useState, useCallback } from 'react';
 import './App.css';  // Importa el archivo CSS para los estilos
@@ -171,4 +125,3 @@ const App = () => {
 };
 
 export default App;
->>>>>>> 35c9b5f292afb60d8d4566794280902d04c5d839
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
@@ -9,18 +8,3 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-
-
->>>>>>> 35c9b5f292afb60d8d4566794280902d04c5d839
